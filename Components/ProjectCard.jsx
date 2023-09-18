a90e01e3a5f940099a8ec30d2d5047998a58b9f3
Taha Shah <tahashah411@gmail.com>
2023-09-18 19:51:59 +0500

    project section and links

--- Components/ProjectCard.jsx
+++ Components/ProjectCard.jsx
@@ -4,15 +4,15 @@ export const ProjectCard = ({project}) => {
 
   
   return (
-    <div className="relative h-72 w-full overflow-y-hidden rounded-xl glow-white border-4 border-black">
-      <div className="h-full w-full bg-neutral-900 peer">
-
+    <a href={project.link} className="relative h-72 w-full overflow-y-hidden rounded-xl glow-white border-4 border-black group">
+      <div className="h-full w-full flex items-center justify-center bg-[#0c0c0c] peer">
+        <p className='text-white font-serif text-4xl group-hover:opacity-30 duration-500'>{project.name}</p>
       </div>
       <div className="absolute bottom-0 w-full h-1/2 translate-y-full bg-black bg-opacity-30 peer-hover:translate-y-0 hover:translate-y-0 text-neutral-50 peer-hover:opacity-100 hover:opacity-100 duration-500">
         <p className="p-4">
           {project.desc}
         </p>
       </div>
-    </div>
+    </a>
   )
 }
--- Components/Projects.jsx
+++ Components/Projects.jsx
@@ -4,16 +4,29 @@ export const Projects = () => {
   const projects = [
     {
       name: "Board-em",
-      desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt quo iure, tempore veniam voluptatum vero autem, atque perspiciatis, quidem repellat facilis nostrum quod unde mollitia minus maxime quos soluta? Fugit?"
+      link: "https://boardem141.netlify.app",
+      desc: "Board-em was my first project using the MERN stack. It is basically a mix between email and X (Twitter) with both a public and private side. I got the opportunity to learn a lot from developing this website about authentication and data handling and how to make a website responsive to data changes."
+    },
+    {
+      name: "Poke-memo",
+      link: "https://poke-memo141.netlify.app",
+      desc: "Poke-memo was my first project using the React. It is a memory-based game about correctly recognizing and memorizing the pokemon you have already selected before. If you 'Catch-em-all!!', you win the game. It is a basic demonstration of how React can make repetitive components easy with the integration of javascript and html in one place"
+    },
+    {
+      name: "Coming Soon",
+      link: "#",
+      desc: "As a fresh full-stack developer, I have a lot to learn and grow. I am constantly working on new projects to showcase my skills, this portfolio being the latest. Soon there will be a project here worth show-casing some aspect of my skill-set, maybe it could be one of the projects you ask me to build."
+    },
+    {
+      name: "Coming Soon",
+      link: "#",
+      desc: "As a fresh full-stack developer, I have a lot to learn and grow. I am constantly working on new projects to showcase my skills, this portfolio being the latest. Soon there will be a project here worth show-casing some aspect of my skill-set, maybe it could be one of the projects you ask me to build."
     }
   ] 
   return (
     <div className="flex flex-col gap-2" id="projects">
       <h2 className="text-white text-3xl font-bold border-b-2 border-white w-max mx-auto p-2">Projects</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-2 md:px-12 xsm:p-6">
-        {projects.map(project => <ProjectCard project={project} key={project.name} />)}
-        {projects.map(project => <ProjectCard project={project} key={project.name} />)}
-        {projects.map(project => <ProjectCard project={project} key={project.name} />)}
         {projects.map(project => <ProjectCard project={project} key={project.name} />)}
       </div>
     </div>
